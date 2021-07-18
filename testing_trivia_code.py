482bdff3d409d5c1aa721765d74975bd1a12c3f0
PXIONG001 <61022244+PXIONG001@users.noreply.github.com>
2021-07-17 22:40:55 -0500

    Peng: Finished my code. This will be a deleted file.

--- testing_trivia_code.py
+++ testing_trivia_code.py
@@ -4,8 +4,25 @@ if file.lower().strip() == exfile[index].lower().strip() and file.lower().strip(
     aQuestion = input('%s: ' % (mQuestion[index]))
     if aQuestion.lower().strip() == mAnswer[index].lower().strip():
         print('correct \n')
-        correct_movies_score += 1
+
+        if file.lower().strip() == 'movie':
+            correct_movies_score += 1
+
+        elif file.lower().strip() == 'history':
+            correct_history_score += 1
+        
+        elif file.lower().strip() == 'sports':
+            correct_sport_score += 1
+
         sum_correct += 1
     else:
         print('Incorrect \n')
-    total_movies_score += 1
+
+        if file.lower().strip() == 'movie':
+            total_movies_score += 1
+
+        elif file.lower().strip() == 'history':
+            total_history_score += 1
+        
+        elif file.lower().strip() == 'sports':
+            total_sport_score += 1
