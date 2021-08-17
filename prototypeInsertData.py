295bdef32c723a7e2fe240f9abe75206e3cf0ba3
PXIONG001 <61022244+PXIONG001@users.noreply.github.com>
2021-08-16 20:51:17 -0500

    Peng: Minor changes.

--- prototypeInsertData.py
+++ prototypeInsertData.py
@@ -1,27 +1,11 @@
-import psycopg2
-from config import config
+from Insert_NewUser import insert_data
+class Account:
+    def __init__(self, name, password) -> None:
+        self.name = name
+        self.password = password
+        
+    def account(self):
+        insert_data(self.name, self.password)
 
-def insert_data(id, name, password):
-    try:
-        params = config()
-        connection = psycopg2.connect(**params)
-        cursor = connection.cursor()
-
-        postgres_insert_query = """ INSERT INTO users("ID", "Name", "Password") VALUES (%s,%s,%s)"""
-        record_to_insert = (9, 'One Plus 6', 'Seven')
-        cursor.execute(postgres_insert_query, record_to_insert)
-
-        connection.commit()
-        count = cursor.rowcount
-        print(count, "Record inserted successfully into mobile table")
-
-    except (Exception, psycopg2.Error) as error:
-        print("Failed to insert record into mobile table", error)
-
-    finally:
-        # closing database connection.
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
-
+account = Account('Amber Jewels', 'Renault')
+print(account.account())
