81c2be28bc194fe1c838f129412fcc38a4ad23a5
PXIONG001 <61022244+PXIONG001@users.noreply.github.com>
2021-08-16 20:50:54 -0500

    Peng: Minor changes.

--- database.py
+++ database.py
@@ -1,37 +1,15 @@
 import psycopg2
-from config import config
+from Insert_NewUser import insert_data
+from Fetch_User import fetch_user
 
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
+def database(user_sign):
+    """ Chooses which procedure to proceed with the database """
+    
+    if user_sign == 'nu':
+        insert_data()
 
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-		
-        # create a cursor
-        cur = conn.cursor()
-        
-	# execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-       
-	# close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
-
+    elif user_sign == 'u':
+        fetch_user()
 
 if __name__ == '__main__':
-    connect()
+    database('nu')
