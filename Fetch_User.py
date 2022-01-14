b783a33e0e5af1b46a37653002ad362d84d3a2a3
PXIONG001 <pengxiong001@gmail.com>
2022-01-14 11:04:11 -0600

    It was originally suppose to be the database connect, but I decided that I wanted to move the connection to a new python file called, ironically, database. This file will be the one to fetch the user in which it calls database.

--- Fetch_User.py
+++ Fetch_User.py
@@ -1,30 +1,9 @@
 import psycopg2
 from config import config
+from database import database
 
 def fetch_user(user):
-    try:
-        params = config()
-        connection = psycopg2.connect(**params)
-        cursor = connection.cursor()
-
-        postgres_insert_query = """ SELECT * FROM users;"""
-        cursor.execute(postgres_insert_query)
-
-        result = cursor.fetchall()
-        print(result)
-        connection.commit()
-
-        connection.close()
-
-    except (Exception, psycopg2.Error) as error:
-        print("Failed to fetch record from users table", error)
-
-    finally:
-        # closing database connection.
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
+    return 0;
 
 if __name__ == '__main__':
     fetch_user('Penguin')
